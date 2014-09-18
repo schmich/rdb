1764085f88d668a747bdc76d18bdc99d18a57c86
Chris Schmich <schmch@gmail.com>
2014-09-18 00:36:33 -0500

    Add command-line option to print version.

--- lib/rdb/web/cli.rb
+++ lib/rdb/web/cli.rb
@@ -5,7 +5,13 @@ module Rdb
   class CommandLine < Thor
     desc '[--host <host>] [--port <port>]', 'Start debug server.'
     options :port => :integer, :host => :string
+    option :version, :type => :boolean, :aliases => :v
     def start
+      if options[:version]
+        version
+        return
+      end
+ 
       bind = options[:host] || 'localhost'
       port = options[:port] || 4567
 
@@ -15,6 +21,11 @@ module Rdb
       server.run! do
         Launchy.open("http://localhost:#{port}")
       end
+    end
+
+    desc '-v|--version', 'Print version.'
+    def version
+      puts "rdb #{Rdb::VERSION}"
     end
 
     default_task :start
